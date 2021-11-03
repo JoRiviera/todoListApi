0e8b8742559300497aef557f8f34249a103519dd
Jo Riviera <guirito.jc@gmail.com>
2021-11-03 21:48:46 +0100

    Edit connection string and await for valid connection to db before starting the app.

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/TodoListDB');
+
+mongoose.connect('mongodb://127.0.0.1:27017/TodoListDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-routes(app);
-app.listen(port);
+(async () => {
+    await routes(app);
+    app.listen(port);
 
-console.log('Your first node api is running on port: ' + port);
+    console.log('Your first node api is running on port: ' + port);
+})();
 
 module.exports = app;
