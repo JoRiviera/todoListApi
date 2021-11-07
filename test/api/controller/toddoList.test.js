ac2f1052fbe2e55109a02eb72a6b2bfe71719500
Jo Riviera <guirito.jc@gmail.com>
2021-11-07 14:25:29 +0100

    EDIT dummyResponse Json

--- test/api/controller/toddoList.test.js
+++ test/api/controller/toddoList.test.js
@@ -21,7 +21,7 @@ const dummyResponse = {
     name: 'DummyResponse',
     content: null,
     callback: null,
-    json: (data) => dummyCallback.call(dummyResponse, data),
+    json: dummyCallback,
 };
 
 const dummyNext = {
