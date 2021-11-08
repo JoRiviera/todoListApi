5ffbbf05ed4308f84e77786a1baa0d070d9ed42f
Jo Riviera <guirito.jc@gmail.com>
2021-11-08 11:36:20 +0100

    ADD: category controller

---
+++ api/controller/categoryList.js
@@ -0,0 +1,38 @@
+'use strict';
+const categorySchema = require('../model/category');
+const mongoose = require('mongoose');
+const CategoryModel = mongoose.model('CategoryModel', categorySchema);
+const { respond } = require('./helpers');
+
+const categoryController = {
+    getAll: (req, res, next) => {
+        CategoryModel.find({}, (err, categories) => {
+            return respond(err, categories, res, next);
+        });
+    },
+    create: (req, res, next) => {
+        const newCategoryItem = new CategoryModel(req.body);
+        newCategoryItem.save((err, savedCategoryItem) => {
+            return respond(err, savedCategoryItem, res, next);
+        });
+    },
+    get: (req, res, next) => {
+        CategoryModel.findById(req.params.id, (err, categoryItem) => {
+            return respond(err, categoryItem, res, next);
+        });
+    },
+    update: (req, res, next) => {
+        CategoryModel.findByIdAndUpdate(req.params.id, req.body,{
+                runValidators: true, returnDocument: 'after'},
+            (err, categoryItem) => {
+                return respond(err, categoryItem, res, next);
+            });
+    },
+    delete: (req, res, next) => {
+        CategoryModel.findByIdAndRemove(req.params.id, (err, categoryItem) => {
+            return respond(err, categoryItem, res, next);
+        });
+    }
+};
+
+module.exports = categoryController;
