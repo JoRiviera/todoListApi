034909c48f8125e789f4ea96eced0fa02f8987af
Jo Riviera <guirito.jc@gmail.com>
2021-11-10 00:38:24 +0100

    EDIT: populate todoitem controller getAll, get, update responses with categories data

--- api/controller/todoList.js
+++ api/controller/todoList.js
@@ -6,7 +6,7 @@ const todoListController = {
   getAll: (req, res, next) => {
     TodoItem.find({}, (err, todoItems) => {
       return respond(err, todoItems, res, next);
-    });
+    }).populate('categories');
   },
   create: (req, res, next) => {
     const newTodoItem = new TodoItem(req.body);
@@ -17,14 +17,14 @@ const todoListController = {
   get: (req, res, next) => {
     TodoItem.findById(req.params.id, (err, todoItem) => {
       return respond(err, todoItem, res, next);
-    });
+    }).populate('categories');
   },
   update: (req, res, next) => {
     TodoItem.findByIdAndUpdate(req.params.id, req.body,{
       runValidators: true, returnDocument: 'after'},
         (err, todoItem) => {
         return respond(err, todoItem, res, next);
-    });
+    }).populate('categories');
   },
   delete: (req, res, next) => {
     TodoItem.findByIdAndRemove(req.params.id, (err, todoItem) => {
