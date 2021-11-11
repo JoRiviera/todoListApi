78d90cb51330fcc53572c548510bf2690a050306
Jo Riviera <guirito.jc@gmail.com>
2021-11-11 22:21:10 +0100

    EDIT: Category model variable name

--- api/controller/categoryList.js
+++ api/controller/categoryList.js
@@ -1,35 +1,35 @@
 'use strict';
-const CategoryModel = require('../model/category').model;
-const TodoItemModel = require('../model/todoItem').model;
+const Category = require('../model/category').model;
+const TodoItem = require('../model/todoItem').model;
 const { respond } = require('./helpers');
 
 const categoryController = {
     getAll: (req, res, next) => {
-        CategoryModel.find({}, (err, categories) => {
+        Category.find({}, (err, categories) => {
             return respond(err, categories, res, next);
         });
     },
     create: (req, res, next) => {
-        const newCategoryItem = new CategoryModel(req.body);
+        const newCategoryItem = new Category(req.body);
         newCategoryItem.save((err, savedCategoryItem) => {
             return respond(err, savedCategoryItem, res, next);
         });
     },
     get: (req, res, next) => {
-        CategoryModel.findById(req.params.id, (err, categoryItem) => {
+        Category.findById(req.params.id, (err, categoryItem) => {
             return respond(err, categoryItem, res, next);
         });
     },
     update: (req, res, next) => {
-        CategoryModel.findByIdAndUpdate(req.params.id, req.body,{
+        Category.findByIdAndUpdate(req.params.id, req.body,{
                 runValidators: true, returnDocument: 'after'},
             (err, categoryItem) => {
                 return respond(err, categoryItem, res, next);
             });
     },
     delete: (req, res, next) => {
-        CategoryModel.findByIdAndRemove(req.params.id, (err, categoryItem) => {
-            TodoItemModel.updateMany({categories: {$all: req.params.id}},
+        Category.findByIdAndRemove(req.params.id, (err, categoryItem) => {
+            TodoItem.updateMany({categories: {$all: req.params.id}},
                 {$pull: {categories: req.params.id}}, (err) => {
                     return respond(err, categoryItem, res, next);
                 });
